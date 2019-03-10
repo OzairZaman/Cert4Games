18e6c6f0405b43bb6e99bdd217527e4f5a881516
Ozair Zaman <ozair.zaman@outlook.com>
2019-03-11 07:33:39 +1100

    New project

---
+++ WayPointsPatrol/Assets/Scripts/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float speed = 10f;
+    private Transform target; // will hold the current waypoint
+    private int currIndex = 0;
+    
+    // Use this for initialization
+	void Start () {
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //call Patrol method
+        Patrol();
+    }
+
+
+    void Patrol()
+    {
+        //target = Waypoints._waypoints[currIndex];
+        //Vector3 dir = target.position - this.transform.position;
+        //this.transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+        this.transform.position = Vector3.MoveTowards(transform.position, Waypoints._waypoints[currIndex].position, speed * Time.deltaTime);
+    }
+}
---
+++ WayPointsPatrol/Assets/Scripts/Waypoints.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour {
+
+    // we are creating a static aray of waypoint transform
+    public static Transform[] _waypoints;
+    
+
+    private void Awake()
+    {
+        
+
+        GetAllWayPoints();
+
+         
+        
+    }
+
+    private void Update()
+    {
+        if (this.transform.childCount > _waypoints.Length)
+        {
+            GetAllWayPoints();
+        }
+    }
+
+    //this method gets all child waypoints and puts
+    //them in the array.
+    void GetAllWayPoints()
+    {
+
+        // set the array to the size of the number of children of this transform
+        // (waypoints will be created as child to the game object that this 
+        // script is attachted to.
+        _waypoints = new Transform[this.transform.childCount];
+
+        //iterate through the array and assign
+        for (int i = 0; i < _waypoints.Length; i++)
+        {
+            _waypoints[i] = this.transform.GetChild(i);
+        }
+
+        print(_waypoints.Length);
+    }
+}
